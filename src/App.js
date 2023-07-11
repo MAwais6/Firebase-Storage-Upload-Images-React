a904d5f1a0ee2a7681a66ac5450f189f88c8b14a
MAwais6 <f200265@cfd.nu.edu.pk>
2023-07-11 17:35:59 +0500

    Upload Files ✅✔

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { storage } from './firebase'
+import { ref, uploadBytes } from 'firebase/storage'
 
 function App() {
+  
+  const [ imageUpload , setImageUpload ] = useState(null)
+
+  const uploadImage = async e =>{
+    
+    if (imageUpload === null) return alert("Please select an image to upload")
+
+    const files = imageUpload 
+    const file = files[0]
+    const imageRef = ref( storage , `images/${file.name}` )
+    uploadBytes(imageRef , file)
+    .then( () => alert("Image uploaded successfully") )
+    .catch( (error) => alert(error.message) )
+    
+  }
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label> Upload Images </label>
+      <input type="file"  onChange={(event) => { setImageUpload(event.target.files) } } />
+      <br></br>
+      <button onClick={uploadImage} > Upload </button>
     </div>
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET ,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
